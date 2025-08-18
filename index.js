26c6c3d0df12bfdf5a4899a0913481cafa140568
Nico Velasco <nicolasvelasco.r@gmail.com>
2025-08-18 00:00:42 -0400

    Cors origin

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ import petRouter from './routes/pets.routes.js'
 import userRouter from './routes/users.routes.js';
 import requestRouter from './routes/request.routes.js';
 
-app.use(cors({}));
+
+app.use(cors({
+  origin: 'https://uhm-front.netlify.app', // o '*' si estás en desarrollo
+  credentials: true
+}));
+
+
 app.use(express.json())
 app.use('/pets', petRouter);
 app.use('/users', userRouter)
 
 const PORT = process.env.PORT || 5000;
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Escuchando puerto ${PORT}`);
