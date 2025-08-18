90aeae21f73c59ac5c7dd90cc9fa1bb12ad7efa0
Nico Velasco <nicolasvelasco.r@gmail.com>
2025-08-18 10:51:17 -0400

    no app.listen

--- index.js
+++ index.js
@@ -7,31 +7,16 @@ import requestRouter from './routes/request.routes.js';
 
 
 app.use(cors({
-  origin: 'https://uhm-front.netlify.app', // o '*' si estás en desarrollo
+  origin: ['https://uhm-front.netlify.app'], 
   credentials: true
 }));
 
 
 app.use(express.json())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://uhm-front.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200); // Responde al preflight
-  }
-
-  next();
-});
 app.use('/pets', petRouter);
 app.use('/users', userRouter)
 const PORT = process.env.PORT || 5000;
 
 
-
-
-app.listen(PORT, () => {
-  console.log(`Escuchando puerto ${PORT}`);
-});
 export default app;
